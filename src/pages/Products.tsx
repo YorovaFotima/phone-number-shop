fe99fac2665bcac879d784fdf99f5294632b93a2
YorovaFotima <erzoda.fotima@gmail.com>
2024-03-21 14:23:54 +0500

    ProductsPage

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,17 +1,42 @@
+import {  useState } from "react";
+import './productsPage.css'
 function ProductsPage() {
   async function handleFetchClick() {
     fetch("https://dummyjson.com/products")
       .then((res) => res.json())
-      .then((json) => console.log(json));
+      .then((json) => setproducts(json.products));
   }
-
+  const [products,setproducts] = useState([]);
+  console.log(products);
+  
   return (
     <div>
       <h1>Products</h1>
 
-      <button onClick={handleFetchClick} style={{ padding: "10px" }}>
+      <button onClick={handleFetchClick} style={{ padding: "10px", backgroundColor: "green", "color": "white" }}>
         Загрузить продукты
       </button>
+       
+      <table >
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+        {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.title}</td>
+              <td>{product.description}</td>
+              <td>{product.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
